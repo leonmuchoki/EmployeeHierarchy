9b4254bd930ba0588a4ed6f2148313162031a578
leon muchoki <muchokileon@gmail.com>
2019-10-03 19:33:32 +0300

    feat: add employee data object

---
+++ EmployeeHierachy/EmployeesHierachy/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesHierachy
+{
+    public class Employee
+    {
+        public string employeeId { get; set; }
+        public string managerId { get; set; }
+        public Decimal salary { get; set; }
+    }
+}
--- EmployeeHierachy/EmployeesHierachy/Employees.cs
+++ EmployeeHierachy/EmployeesHierachy/Employees.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EmployeesHierachy
 {
     public class Employees
     {
+        string _csv_file_path;
+        char _csv_separator;
+        public Employees(string csv_path, string csvSeparator)
+        {
+            _csv_file_path = csv_path;
+            _csv_separator = char.Parse(csvSeparator);
+        }
+
+        public void ReadCSVLine()
+        {
+            using (var reader = new StreamReader(_csv_file_path))
+            {
+                List<string> listEmpIds = new List<string>();
+                List<string> listManagerIds = new List<string>();
+                List<decimal> listSalaries = new List<decimal>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(_csv_separator);
+
+                    listEmpIds.Add(values[0]);
+                    listManagerIds.Add(values[1]);
+                    listSalaries.Add(decimal.Parse(values[2]));
+                }
+            }
+        }
+
     }
 }
